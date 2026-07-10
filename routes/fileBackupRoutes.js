c3056f55dbdbd1b0332f397d2da156f6ccb59577
Yusuf Burhani <yusufburhani20@github.com>
2026-07-10 15:06:55 +0700

    Fix middleware import

--- routes/fileBackupRoutes.js
+++ routes/fileBackupRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const fileBackupController = require('../controllers/fileBackupController');
-const { verifyToken } = require('../middleware/authMiddleware');
+const { requireAuth } = require('../middleware/auth');
 
 // Semua endpoint wajib login
-router.use(verifyToken);
+router.use(requireAuth);
 
 router.get('/list', fileBackupController.listFiles);
 router.get('/download', fileBackupController.downloadFile);
